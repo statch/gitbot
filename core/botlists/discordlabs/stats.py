d20ee2c3b9947a338cd1d279251663cef97f3371
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-21 16:29:35 +0100

    Update stats.py

--- core/botlists/discordlabs/stats.py
+++ core/botlists/discordlabs/stats.py
@@ -7,7 +7,7 @@ class DiscordLabsStats(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.token: str = getenv("DISCORDLABS")
-        self.discord_labs = dlabs.Client(bot=self.client, token=self.token, autopost=True)
+        self.discord_labs = dlabs.Client(bot=self.client, token=self.token, autopost=True, verbose=True)
 
 
 def setup(client):

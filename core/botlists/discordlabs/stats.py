7c3d8142bb83f1eacdb139653d186d3012e741f2
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-21 16:26:44 +0100

    try to use my trash package

--- core/botlists/discordlabs/stats.py
+++ core/botlists/discordlabs/stats.py
@@ -1,28 +1,13 @@
-import aiohttp
-from discord.ext import commands, tasks
+from discord.ext import commands
 from os import getenv
+import dlabs
 
 
 class DiscordLabsStats(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.token: str = getenv("DISCORDLABS")
-        self.post_dlabs_stats.start()
-
-    @tasks.loop(minutes=30)
-    async def post_dlabs_stats(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.post(f"https://bots.discordlabs.org/v2/bot/{self.client.user.id}/stats",
-                                    data={"token": self.token, "server_count": len(self.client.guilds)}) as res:
-                res = await res.json()
-                if str(res["error"]).lower() != "false":
-                    print(f"\nbots.discordlabs API error:\n\n{res}\n")
-                else:
-                    print("bots.discordlabs stats posted successfully")
-
-    @post_dlabs_stats.before_loop
-    async def wait_until_ready(self):
-        await self.client.wait_until_ready()
+        self.discord_labs = dlabs.Client(bot=self.client, token=self.token, autopost=True)
 
 
 def setup(client):

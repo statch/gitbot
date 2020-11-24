0756d06d6f8d4baeea925ce58978503f6f73c506
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-24 18:32:07 +0100

    Event dispatcher

--- cogs/debug.py
+++ cogs/debug.py
@@ -10,6 +10,24 @@ class Debug(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.emoji: str = '<:github:772040411954937876>'
+        self.e: str = "<:ge:767823523573923890>"
+
+    @is_me()
+    @commands.command(name='dispatch', aliases=['--event', '--dispatch', 'event'])
+    async def manually_trigger_event(self, ctx: commands.Context,  event: str):
+        event = event.lower().replace('on_', "", 1)
+        cor = {
+            "guild_join": ctx.guild,
+            "guild_remove": ctx.guild,
+            "member_join": ctx.author,
+            "member_remove": ctx.author
+        }
+        if cor.get(event, None) is not None:
+            e = cor.get(event, None)
+            self.client.dispatch(event, e)
+            await ctx.send(f"{self.emoji} Dispatched event `{event}`")
+        else:
+            await ctx.send(f"{self.e}  Failed to dispatch event `{event}`")
 
     @is_me()
     @commands.command(aliases=["--rate"])

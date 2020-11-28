e3ca0f4db31d4bcccb672b2e54a4e40ca6e47e31
Paul Przybyszewski <63970738+itsmewulf@users.noreply.github.com>
2020-11-28 22:41:54 +0100

    why did i do that in the first place

--- ext/manager.py
+++ ext/manager.py
@@ -22,14 +22,10 @@ class Manager:
         self.emojis = json_dict("emoji")
 
     def correlate_license(self, to_match: str) -> dict or None:
-        for i in [x for x in self.licenses]:
+        for i in list(self.licenses):
             match = fuzz.token_set_ratio(to_match, i["name"])
-            if match >= 80:
-                return [y for y in self.licenses if y["name"] == i["name"]][0]
             match1 = fuzz.token_set_ratio(to_match, i["key"])
-            if match1 >= 80:
-                return [y for y in self.licenses if y["key"] == i["key"]][0]
             match2 = fuzz.token_set_ratio(to_match, i["spdx_id"])
-            if match2 >= 80:
-                return [y for y in self.licenses if y["spdx_id"] == i["spdx_id"]][0]
+            if any([match > 80, match1 > 80, match2 > 80]):
+                return i
         return None

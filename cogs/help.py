7ccd1ec69156971eed6671f4f388ae4acffcd590
itsmewulf <pa.przybyszewski@gmail.com>
2021-01-03 20:27:46 +0100

    aliases and typings

--- cogs/help.py
+++ cogs/help.py
@@ -8,14 +8,14 @@ Git = globals.Git
 
 class Help(commands.Cog):
     def __init__(self, client):
-        self.client = client
+        self.client: commands.Bot = client
         self.e: str = "<:ge:767823523573923890>"
         self.ga: str = "<:ga:768064843176738816>"
 
     @guild_available()
     @commands.group(name='--help', aliases=['help', '-H'])
     @commands.cooldown(15, 30, commands.BucketType.user)
-    async def help_command(self, ctx) -> None:
+    async def help_command(self, ctx: commands.Context) -> None:
         if ctx.invoked_subcommand is None:
             lines: list = ["In this section you'll find info and usage of my commands.",
                            "\n**You can access specific parts by typing:**",
@@ -35,7 +35,7 @@ class Help(commands.Cog):
     @guild_available()
     @help_command.command(name='checkout', aliases=['-checkout', '--checkout'])
     @commands.cooldown(15, 30, commands.BucketType.user)
-    async def checkout_help(self, ctx) -> None:
+    async def checkout_help(self, ctx: commands.Context) -> None:
         lines: list = ["The checkout command allows you to fetch information directly from GitHub.",
                        "\n**All commands listed below assume being prefixed with** `git checkout`",
                        "Words in curly braces symbolize arguments that the command requires",
@@ -43,7 +43,8 @@ class Help(commands.Cog):
                        "`--user -repos {username}` - get user's first 15 repos and a link to more",
                        "`--org -info {organization}` - get info about an organization",
                        "`--org -repos {organization}` - get organization's first 15 repos and a link to more",
-                       "\n**Important!** Repo commands that follow, require the exact syntax of `username/repo-name` in place of the `{repo}` argument, ex. `itsmewulf/GitHub-Discord`",
+                       "\n**Important!** Repo commands that follow, require the exact syntax of `username/repo-name` "
+                       "in place of the `{repo}` argument, ex. `itsmewulf/GitHub-Discord`",
                        "\n`--issue {repo} {issue number}` - get detailed info on an issue",
                        "`--repo -info {repo}` - get info about a repository",
                        "`--repo -src {repo}` - get the repo's file structure"]
@@ -58,7 +59,7 @@ class Help(commands.Cog):
     @guild_available()
     @help_command.command(name='utility', aliases=['-utility', '--utility'])
     @commands.cooldown(15, 30, commands.BucketType.user)
-    async def utlity_help(self, ctx) -> None:
+    async def utlity_help(self, ctx: commands.Context) -> None:
         lines: list = ["These commands let you fetch various data related to Git and GitHub.",
                        "`git info --license {license}` - get info about a license",
                        "`git --download {repo}` - get a handy zip with the source code of the repo",
@@ -74,7 +75,7 @@ class Help(commands.Cog):
     @guild_available()
     @help_command.command(name='config', aliases=['-config', '--config'])
     @commands.cooldown(15, 30, commands.BucketType.user)
-    async def config_help(self, ctx) -> None:
+    async def config_help(self, ctx: commands.Context) -> None:
         lines: list = ["**These commands affect the behavior of the Bot.**",
                        "`git config` - get detailed info on your options",
                        "`git config -show` - shows your current settings"]
@@ -89,7 +90,7 @@ class Help(commands.Cog):
     @guild_available()
     @help_command.command(name="info", aliases=["-info", "--info"])
     @commands.cooldown(15, 30, commands.BucketType.user)
-    async def info_help(self, ctx):
+    async def info_help(self, ctx: commands.Context):
         lines: list = ["These commands have no ties to GitHub and focus on the Bot itself.",
                        "`git --aliases` - get a list of command shorthands",
                        "`git --privacy` - the Bot's privacy policy",
@@ -109,14 +110,14 @@ class Help(commands.Cog):
     @guild_available()
     @commands.group(name='--aliases', aliases=['aliases'])
     @commands.cooldown(15, 30, commands.BucketType.user)
-    async def alias_command(self, ctx):
+    async def alias_command(self, ctx: commands.Context):
         if ctx.invoked_subcommand is None:
             lines: list = ["In this section you'll find shorthands of my commands",
                            "\n**You can access specific parts by typing:**",
                            "`git --aliases checkout` - for checkout command aliases",
-                           "`git --aliases info` - for information fetching aliases",
+                           "`git --aliases info` - for other aliases",
                            "`git --aliases config` - for configuration command aliases",
-                           "`git --aliases utility` - for other aliases"]
+                           "`git --aliases utility` - for utility command aliases"]
             embed = discord.Embed(
                 title=f"{self.e}  Aliases",
                 color=0xefefef,
@@ -128,7 +129,7 @@ class Help(commands.Cog):
     @alias_command.command(name="checkout", aliases=['-checkout', '--checkout'])
     @commands.cooldown(15, 30, commands.BucketType.user)
     @guild_available()
-    async def checkout_aliases(self, ctx):
+    async def checkout_aliases(self, ctx: commands.Context):
         lines: list = ["**All commands listed below begin with** `git checkout` **or** `git C`",
                        f"`--user -info` {self.ga} `-U -I`",
                        f"`--user -repos` {self.ga} `-U -R`",
@@ -148,14 +149,15 @@ class Help(commands.Cog):
     @alias_command.command(name="utility", aliases=['-utility', '--utility'])
     @commands.cooldown(15, 30, commands.BucketType.user)
     @guild_available()
-    async def utility_aliases(self, ctx):
+    async def utility_aliases(self, ctx: commands.Context):
         lines: list = [
-            "Shorthands for commands not tied to GitHub itself",
-            f"`git --uptime` {self.ga} `git --up`",
-            f"`git --ping` {self.ga} `git --p`"
+            "**Shorthands for commands used to fetch data related to Git and GitHub**",
+            f"`git info --license` {self.ga} `git info -L`",
+            f"`git --download` {self.ga} `git -dl`",
+            f"`git --lines` {self.ga} `git -L`"
         ]
         embed = discord.Embed(
-            title=f"{self.e}  Other Aliases",
+            title=f"{self.e}  Utility Aliases",
             color=0xefefef,
             description="\n".join(lines)
         )
@@ -165,7 +167,7 @@ class Help(commands.Cog):
     @alias_command.command(name="config", aliases=['-config', '--config'])
     @commands.cooldown(15, 30, commands.BucketType.user)
     @guild_available()
-    async def config_aliases(self, ctx):
+    async def config_aliases(self, ctx: commands.Context):
         lines: list = [
             "Shorthands for commands used to store your preferred orgs, repos and users",
             f"`git config` {self.ga} `git -cfg`",
@@ -185,11 +187,11 @@ class Help(commands.Cog):
     @guild_available()
     @alias_command.command(name="info", aliases=["-info", "--info"])
     @commands.cooldown(15, 30, commands.BucketType.user)
-    async def info_command_aliases(self, ctx):
+    async def info_command_aliases(self, ctx: commands.Context):
         lines: list = [
-            "**Shorthands for commands used to fetch data related to Git and GitHub**",
-            f"`git info --license` {self.ga} `git info -L`",
-            f"`git --lines` {self.ga} `git -L`"
+            "Shorthands for commands not tied to GitHub itself",
+            f"`git --uptime` {self.ga} `git --up`",
+            f"`git --ping` {self.ga} `git --p`"
         ]
         embed = discord.Embed(
             title=f"{self.e}  Info Aliases",

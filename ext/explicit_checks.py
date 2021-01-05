ffcd166590dbefd689f1b0364ed032096c9528fb
itsmewulf <pa.przybyszewski@gmail.com>
2021-01-05 22:11:25 +0100

    codefactor changes

--- ext/explicit_checks.py
+++ ext/explicit_checks.py
@@ -1,14 +1,13 @@
 import discord
 
+SEND_REQUIREMENTS: list = [("send_messages", True), ("read_messages", True), ("read_message_history", True)]
+
 
 async def verify_send_perms(channel: discord.TextChannel) -> bool:
     if isinstance(channel, discord.DMChannel):
         return True
     perms: list = list(iter(channel.permissions_for(channel.guild.me)))
     overwrites: list = list(iter(channel.overwrites_for(channel.guild.me)))
-    checks: list = ["send_messages", "read_messages", "read_message_history"]
-    reqs: list = [(req, True) for req in checks]
-    if all(x in perms for x in reqs) or all(x in overwrites for x in reqs) or ("administrator", True) in perms:
+    if all(req in perms + overwrites for req in SEND_REQUIREMENTS) or ("administrator", True) in perms:
         return True
-    else:  # Can't send a message
-        return False
+    return False

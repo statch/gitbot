bfdbc42922769766e270124df4bb07d86ee589af
itsmewulf <pa.przybyszewski@gmail.com>
2021-04-05 00:33:38 +0200

    some a e s t h e t i c DictProxy changes

--- ext/structs/proxies/dict_proxy.py
+++ ext/structs/proxies/dict_proxy.py
@@ -13,9 +13,10 @@ class DictProxy(dict):
     def __init__(self, data: Union[dict, list]):
         self.__items: Union[list, dict] = data
         if isinstance(data, dict):
-            super().__init__(data)
-            for k, v in data.items():
-                setattr(self, k, (v if not isinstance(v, dict) else DictProxy(v)))
+            super().__init__({k: v if not isinstance(v, dict) else DictProxy(v) for k, v in data.items()})
+
+    __getattr__ = dict.__getitem__
+    __delattr__ = dict.__delitem__
 
     def __iter__(self):
         yield from self.__items
@@ -23,5 +24,5 @@ class DictProxy(dict):
     def __getitem__(self, item: Union[str, int]) -> Any:
         return self.__items[item]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: Any) -> None:
         setattr(self, key, DictProxy(value) if isinstance(value, dict) else value)

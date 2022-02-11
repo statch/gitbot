3fb95a3f0af56de54ec89d15d9799ef92cebc624
Paul Przybyszewski <pa.przybyszewski@gmail.com>
2022-02-11 22:57:58 +0100

    codebase: top.gg?

---
+++ cogs/botlists/major/topgg_.py
@@ -0,0 +1,18 @@
+import topgg
+from discord.ext import commands
+from os import getenv
+
+
+class TopGGStats(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
+        self.token: str = getenv("TOPGG")
+        self.gg: topgg.DBLClient = topgg.DBLClient(bot, self.token, autopost=True)
+
+    @commands.Cog.listener()
+    async def on_autopost_success(self):
+        print("Successfully posted stats to top.gg")
+
+
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(TopGGStats(bot))
